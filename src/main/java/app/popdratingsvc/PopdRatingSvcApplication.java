006b13d950b305a29b3e3fa46bdbf950625336c8
Tsvetelina Gadzhelova <tsvetelina.gadzhelova@gmail.com>
2025-11-12 14:45:16 +0200

    chore: initial Spring Boot project setup

---
+++ src/main/java/app/popdratingsvc/PopdRatingSvcApplication.java
@@ -0,0 +1,13 @@
+package app.popdratingsvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopdRatingSvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PopdRatingSvcApplication.class, args);
+    }
+
+}
